8c7f444d503f3823c4c7bd68abde6585d1b08e18
bigwave-cho <hooyaa93@gmail.com>
2023-03-29 23:23:01 +0900

    bottomNavigationBar : index 따라 화면 전환 & 탭에 따른 네비 배경 변화

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex], // index에 따라 보여지는 화면
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        // 배경색 변화에 대한 타입 지정
+        // fixed 배경색 안바뀜
+        // shifting 바뀜
+        currentIndex: _selectedIndex,
+        onTap: _onTap, // tap하면 index를 arg로 전달
+        // selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          // Nav 아이템은 2개 이상
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "WW?",
+            backgroundColor: Colors.amber,
+            //선택됐을 때 nav의 배경색.
+            // 주의: 4개 이상일 때 자동 활성화됨
+            // 강제 적용할 수 있는 방법도 있음.
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "WW?",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "WW?",
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "WW?",
+            backgroundColor: Colors.yellow,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "WW?",
+            backgroundColor: Colors.teal,
+          ),
+        ],
+      ),
+    );
   }
 }
