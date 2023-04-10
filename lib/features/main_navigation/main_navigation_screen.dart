d82db054cfe40c65e873815c97d18da0ccf287a6
bigwave-cho <hooyaa93@gmail.com>
2023-04-10 09:58:58 +0900

    Custom NavBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -36,21 +40,60 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Cupertino style이지만 완전히 애플 스타일로 만들고 싶다면
-    // main의 MaterialApp을 CupertinoApp으로 바꿔야 함.
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.home),
-            label: 'Home',
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                //bottomNavBar에 Column이 들어가게 되면
+                //화면 전체를 덮어버리기 때문에
+                // mainAxisSize를 최소로 설정
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  FaIcon(
+                    FontAwesomeIcons.house,
+                    color: Colors.white,
+                  ),
+                  Gaps.v5,
+                  Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+              // 위 Column을 위젯화
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.search), label: "Search"),
-        ],
+        ),
       ),
-      // tabBuilder : 해당 index 스크린
-      tabBuilder: (context, index) => screens[index],
     );
   }
 }
