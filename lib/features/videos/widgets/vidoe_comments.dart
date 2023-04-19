dce51f58e768465d175a52c9ffce0d0bcc1ffb1c
bigwave-cho <hooyaa93@gmail.com>
2023-04-19 23:11:51 +0900

    Comments: comment

--- lib/features/videos/widgets/vidoe_comments.dart
+++ lib/features/videos/widgets/vidoe_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -38,11 +39,74 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: ((context, index) => Container(
-                  child: const Text('commnets'),
-                ))),
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v10,
+          itemCount: 10,
+          itemBuilder: ((context, index) => Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 18,
+                    child: Text(
+                      'JH',
+                    ),
+                  ),
+                  Gaps.h10,
+                  Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'JH',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Gaps.v3,
+                      const Text(
+                        "that that that dkdkdkddkdkd slkfanlsdf sldkfnasdfkjlndslf lsadkfn!!",
+                      )
+                    ],
+                  )),
+                  Gaps.h10,
+                  Column(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size20,
+                        color: Colors.grey.shade500,
+                      ),
+                      Gaps.v2,
+                      Text(
+                        '52.2k',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(children: [
+            CircleAvatar(
+              radius: 18,
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.grey.shade500,
+              child: const Text(
+                'JH',
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
