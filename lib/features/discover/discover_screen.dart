b75477f97d8d2a059075901f83a0def68547aae2
bigwave-cho <hooyaa93@gmail.com>
2023-04-20 23:28:08 +0900

    TabBar : 탭 많이 만들고 스타일 주는 방법, PreferredSizeWidget

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,17 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+final tabs = ["Top", "Users", "Videos", "Sounds", "LIVE", "Shopping", "Brands"];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Discover",
+    /*
+    TabBar 위젯 사용할 때 뜨는 에러 문구
+    When creating a TabBar, you must either provide an explicit TabController using the "controller" property, or you must ensure that there is a DefaultTabController above the TabBar.
+
+    해결: DefaultTabController위젯으로 감싸기
+     */
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        // AppBar는 preferredSizWidget으로 특정 크기를 가지려하지만
+        // 자식도 prefeerdSizeWidget이어야 하지만
+        // 해당 위젯이 아닌 예시) Container의 경우라도
+        // PrefferdWidgetSize 위젯으로 감싸면 사용가능
+        appBar: AppBar(
+          title: const Text("Discover"),
+          elevation: 1,
+          bottom: TabBar(
+            //splashFactory :TabBar 클릭 애니메이션 설정 : NoSplach 물결 없앰
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            // TabBar는 가지수 많으면 스크롤러블하게 바꿀 수도 있네
+            isScrollable: true,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            indicatorColor: Colors.black,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                )
+            ],
+          ),
         ),
+        body: TabBarView(children: [
+          for (var tab in tabs)
+            Center(
+              child: Text(
+                tab,
+                style: const TextStyle(
+                  fontSize: Sizes.size28,
+                ),
+              ),
+            )
+        ]),
       ),
     );
   }
