bb033be0f566ff2c823651ec7a907bc9a6e6f63a
bigwave-cho <hooyaa93@gmail.com>
2023-04-22 11:45:08 +0900

    Activity: Dismissed Widget part 2

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -3,11 +3,25 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
-class ActivityScreen extends StatelessWidget {
+class ActivityScreen extends StatefulWidget {
   const ActivityScreen({super.key});
 
   @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  // 배열만드는 메서드 List.generate
+  final List<String> _notifications = List.generate(20, (index) => "${index}h");
+
+  void _onDismissed(String notification) {
+    _notifications.remove(notification);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    debugPrint("$_notifications"); // 배열 사라지는거 확인가능.
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -30,88 +44,88 @@ class ActivityScreen extends StatelessWidget {
             ),
           ),
           Gaps.v14,
-          //Dismissible : 옆으로 슬라이드하면 해당 위젯이 화면 상에서 사라짐.
-          // but 현재 상황 : 화면상에서만 사라진거지 위젯트리는 남아있어서
-          /// 플러터는 위젯트리에서도 지우라고 에러를 띄우는 중이다.
-          Dismissible(
-            key: const Key("x"), //임시로 아무 키나 할당.
-            background: Container(
-              alignment: Alignment.centerLeft,
-              color: Colors.green,
-              child: const Padding(
-                padding: EdgeInsets.only(
-                  left: Sizes.size10,
-                ),
-                child: FaIcon(
-                  FontAwesomeIcons.checkDouble,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            secondaryBackground: Container(
-              alignment: Alignment.centerRight,
-              color: Colors.red,
-              child: const Padding(
-                padding: EdgeInsets.only(
-                  right: Sizes.size10,
-                ),
-                child: FaIcon(
-                  FontAwesomeIcons.trashCan,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            child: ListTile(
-              leading: Container(
-                width: Sizes.size52,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.grey.shade400,
-                    width: Sizes.size1,
+          for (var notification in _notifications)
+            Dismissible(
+              key: Key(notification), //임시로 아무 키나 할당.
+              onDismissed: ((direction) => _onDismissed(notification)),
+              background: Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.green,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    left: Sizes.size10,
                   ),
-                ),
-                child: const Center(
                   child: FaIcon(
-                    FontAwesomeIcons.bell,
-                    color: Colors.black,
+                    FontAwesomeIcons.checkDouble,
+                    color: Colors.white,
+                    size: Sizes.size32,
                   ),
                 ),
               ),
-              title: RichText(
-                text: TextSpan(
-                  text: "Account updates:",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                    fontSize: Sizes.size16,
+              secondaryBackground: Container(
+                alignment: Alignment.centerRight,
+                color: Colors.red,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    right: Sizes.size10,
                   ),
-                  children: [
-                    const TextSpan(
-                      text: " Upload longer videos",
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    TextSpan(
-                      text: " 1h",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ],
+                  child: FaIcon(
+                    FontAwesomeIcons.trashCan,
+                    color: Colors.white,
+                    size: Sizes.size32,
+                  ),
                 ),
               ),
-              trailing: const FaIcon(
-                FontAwesomeIcons.chevronRight,
-                size: Sizes.size16,
+              child: ListTile(
+                minVerticalPadding: Sizes.size16,
+                leading: Container(
+                  width: Sizes.size52,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: Sizes.size1,
+                    ),
+                  ),
+                  child: const Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.bell,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                title: RichText(
+                  text: TextSpan(
+                    text: "Account updates:",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                      fontSize: Sizes.size16,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: " Upload longer videos",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " $notification",
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                trailing: const FaIcon(
+                  FontAwesomeIcons.chevronRight,
+                  size: Sizes.size16,
+                ),
               ),
             ),
-          ),
         ],
       ),
     );
