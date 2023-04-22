90cda07f970dd7cc55a447d2ce172c079db627e1
bigwave-cho <hooyaa93@gmail.com>
2023-04-22 22:54:26 +0900

    date, time, range picker

--- lib/features/settings/settings_screent.dart
+++ lib/features/settings/settings_screent.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -15,26 +14,43 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            //showAboutDialog() 내장함수 . 앱에 대한 정보를 띄울 수 있음.
-            //오픈소스라이브러리에 대한 정보도 다 구현돼있어서 일일이 목록 만들 필요 없어짐
-            onTap: (() => showAboutDialog(
-                  context: context,
-                  applicationVersion: "1.0",
-                  applicationLegalese: "All rights reserved.",
-                  applicationName: "TIKUTOKU",
-                  applicationIcon: const FaIcon(
-                    FontAwesomeIcons.cableCar,
-                  ),
-                )),
-            title: const Text(
-              "About",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: const Text("About this app..."),
+            onTap: () async {
+              //날짜피커
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1987),
+                lastDate: DateTime(2030),
+              );
+              print(date); //2023-04-22 00:00:00.000
+
+              //시간피커
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time); //TimeOfDay(22:46)
+
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1987),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text("What is your birthday?"),
           ),
-          // AboutListTile: 함수 만들필요 없음 걍 클릭 ㄱㄴ
           const AboutListTile(applicationName: "TIKUTOKU"),
         ],
       ),
