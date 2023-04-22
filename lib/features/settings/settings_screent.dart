c3ec6310ce2fe6019911020d7ecfa0e4859af4d6
bigwave-cho <hooyaa93@gmail.com>
2023-04-22 23:04:14 +0900

    Switch, Checkbox

--- lib/features/settings/settings_screent.dart
+++ lib/features/settings/settings_screent.dart
@@ -1,7 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +27,36 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          //이거 추천함. ㅋ
+          SwitchListTile.adaptive(
+              value: _notifications, onChanged: _onNotificationsChanged),
+          // 플랫폼에 따라 스이치 변경
+          Switch.adaptive(
+              value: _notifications, onChanged: _onNotificationsChanged),
+          //애플스타일 스위치
+          CupertinoSwitch(
+              value: _notifications, onChanged: _onNotificationsChanged),
+          SwitchListTile(
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+              title: const Text("Enable notifications")),
+          //안드스타일 스위치
+          Switch(value: _notifications, onChanged: _onNotificationsChanged),
+          //그냥 체크박스
+          Checkbox(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+          ),
+          //리스트타일 스타일 체크박스
+          CheckboxListTile(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text(
+              "Enable notifications",
+            ),
+            checkColor: Colors.white,
+            activeColor: Colors.black,
+          ),
           ListTile(
             onTap: () async {
               //날짜피커
