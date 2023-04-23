452881ca49106c9fb46e25c005aafa5f735db294
bigwave-cho <hooyaa93@gmail.com>
2023-04-24 00:45:06 +0900

    Camera: controller init 및 스크린에 띄우기

---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,111 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+  bool _permissionDenied = false;
+
+  late final CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras(); //camera list
+
+    if (cameras.isEmpty) return;
+
+    // debugPrint('$cameras'); 첫번째가 back 두번째가 front임. 확인!
+    //CameraController(description, resolutionPreset)
+    // des : 카메라리스트 중 하나 골라
+    // res : 화질 프리셋
+    _cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.ultraHigh,
+      imageFormatGroup: ImageFormatGroup.bgra8888,
+    );
+
+    await _cameraController.initialize();
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+    //안드는 거절하면 매번 다시 요청
+    // ios는 한번 끝. 나중엔 설정가서 허용해야함.
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+    print('hiㄴㄴ');
+
+    if (!cameraDenied && !micDenied) {
+      print('hi');
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    } else {
+      _permissionDenied = true;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: _permissionDenied
+            ? Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "camera permission is required.",
+                    ),
+                  ),
+                ],
+              )
+            : !_hasPermission || !_cameraController.value.isInitialized
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Initiallizing",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Sizes.size20,
+                        ),
+                      ),
+                      Gaps.v20,
+                      CircularProgressIndicator.adaptive(),
+                    ],
+                  )
+                : Stack(
+                    // 꽉채우려고 넣었는데 비율이 안맞게 늘어남.. ㅋㅋ
+                    // fit: StackFit.expand,
+                    alignment: Alignment.center,
+                    children: [
+                      CameraPreview(_cameraController),
+                    ],
+                  ),
+      ),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,7 @@ import 'package:tiktok/features/authentication/email_screen.dart';
 import 'package:tiktok/features/authentication/sign_up_screen.dart';
 import 'package:tiktok/features/authentication/username_screen.dart';
 import 'package:tiktok/features/users/user_profile_screen.dart';
+import 'package:tiktok/features/videos/video_recording_screen.dart';
 
 /*
 ## Nested Route
@@ -16,74 +17,8 @@ import 'package:tiktok/features/users/user_profile_screen.dart';
 final router = GoRouter(
   routes: [
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: ((context, state) => const SignUpScreen()),
-      routes: [
-        GoRoute(
-          // SignUp이 "/" ->  '/username' 에 해당
-          name: UsernameScreen.routeName,
-          path: UsernameScreen.routeURL,
-          builder: (context, state) => const UsernameScreen(),
-          routes: [
-            GoRoute(
-              name: EmailScreen.routeName,
-              path: EmailScreen.routeURL,
-              builder: ((context, state) {
-                final args = state.extra as EmailScreenArgs;
-                return EmailScreen(username: args.username);
-              }),
-            ),
-          ],
-        ),
-      ],
-    ),
-    // GoRoute(
-    //   path: LoginScreen.routeName,
-    //   builder: ((context, state) => const LoginScreen()),
-    // ),
-    // GoRoute(
-    //   //이름 설정을 해주면
-    //   //context.pushName("username_screen") <- 작동함
-    //   name: "username_screen",
-    //   path: UsernameScreen.routeName,
-    //   // GoRoute transition 적용
-    //   pageBuilder: (context, state) {
-    //     return CustomTransitionPage(
-    //       child: const UsernameScreen(),
-    //       transitionsBuilder: ((context, animation, secondaryAnimation, child) {
-    //         return FadeTransition(
-    //           opacity: animation,
-    //           child: ScaleTransition(
-    //             scale: animation,
-    //             child: child,
-    //           ),
-    //         );
-    //       }),
-    //     );
-    //   },
-    // ),
-    // GoRoute(
-    //   path: EmailScreen.routeName,
-    //   builder: ((context, state) {
-    //     // obj라 프로퍼티에 쉽게 접근할 수 있도록 assertion
-    //     final args = state.extra as EmailScreenArgs;
-
-    //     return EmailScreen(username: args.username);
-    //   }),
-    // ),
-    GoRoute(
-      path: '/users/:username',
-      builder: (context, state) {
-        final username = state.params['username'];
-
-        //쿼리 가져오는 방법
-        final tab = state.queryParams['show'];
-        return UserProfileScreen(
-          username: '$username',
-          tab: tab!,
-        );
-      },
-    ),
+      path: "/",
+      builder: (context, state) => const VideoRecordingScreen(),
+    )
   ],
 );
