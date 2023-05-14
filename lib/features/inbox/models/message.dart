d2d14ce0cc99dc37ebb82fa27a5955cf27a82c9e
bigwave-cho <hooyaa93@gmail.com>
2023-05-14 20:53:47 +0900

    Message: Stream Provider

--- lib/features/inbox/models/message.dart
+++ lib/features/inbox/models/message.dart
@@ -1,20 +1,24 @@
 class MessageModel {
   final String text;
   final String userId;
+  final int createAt;
 
   MessageModel({
     required this.text,
     required this.userId,
+    required this.createAt,
   });
 
   MessageModel.fromJson(Map<String, dynamic> json)
       : text = json['text'],
+        createAt = json['createAt'],
         userId = json['userId'];
 
   Map<String, dynamic> toJson() {
     return {
       "text": text,
       "userId": userId,
+      "createdAt": createAt,
     };
   }
 }
--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok/features/authentication/repos/authentication_repo.dart';
@@ -19,7 +20,11 @@ class MessagesViewModel extends AsyncNotifier<void> {
 
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      final message = MessageModel(text: text, userId: user!.uid);
+      final message = MessageModel(
+        text: text,
+        userId: user!.uid,
+        createAt: DateTime.now().millisecondsSinceEpoch,
+      );
       _repo.sendMessage(message);
     });
   }
@@ -27,4 +32,26 @@ class MessagesViewModel extends AsyncNotifier<void> {
 
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   () => MessagesViewModel(),
+);
+
+final chatProvider = StreamProvider<List<MessageModel>>(
+  (ref) {
+    final db = FirebaseFirestore.instance;
+
+    return db
+        .collection('chat_rooms')
+        .doc("ab0M7ht1Risa95L2UB79")
+        .collection("texts")
+        .orderBy('createdAt')
+        .snapshots()
+        .map(
+          (event) => event.docs
+              .map(
+                (doc) => MessageModel.fromJson(
+                  doc.data(),
+                ),
+              )
+              .toList(),
+        );
+  },
 );
